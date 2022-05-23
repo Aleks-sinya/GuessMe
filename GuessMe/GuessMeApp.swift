fa6f0df0895f0969d444b050cd56b150e8fddfbf
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-05-23 21:08:58 +0700

    Initial Commit

---
+++ GuessMe/GuessMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessMeApp.swift
+//  GuessMe
+//
+//  Created by Алексей Синяговский on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
